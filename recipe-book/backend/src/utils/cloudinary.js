aed51ba36d720f566d5e11a8f842d9017661d126
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-12-06 10:17:38 +0500

    feat: add multer middleware for file uploads and implement Cloudinary upload utility

--- recipe-book/backend/src/utils/cloudinary.js
+++ recipe-book/backend/src/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import {v2 as cloudinary} from cloudinary
+import fs from 'fs'
 
 dotenv.config();
 
@@ -9,3 +10,19 @@ cloudinary.config({
     api_secret : CLOUDINARY_API_SECRET,
 
 })
+
+const uploadToCloudinary = async(localFilePath)=>{
+    try {
+        if(!localFilePath)return null;
+        const response = await cloudinary.uploader.upload(localFilePath , {resource_type : 'auto'})
+        console.log('file upload to cloudinary successfully ' + response.url)
+        fs.unlinkSync(localFilePath)
+        return response;
+    } catch (error) {
+        console.log('file upload failed to cloudinary');
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export {uploadToCloudinary};
