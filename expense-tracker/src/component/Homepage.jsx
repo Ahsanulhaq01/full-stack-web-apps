49bcd5ff2a83f4b8dbc08fba3dd8ba9d34345593
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2026-04-09 15:22:49 +0500

    feat: implement localStorage for expense tracking and enhance UI styling

--- expense-tracker/src/component/Homepage.jsx
+++ expense-tracker/src/component/Homepage.jsx
@@ -1,23 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./homepage.css";
 
 function Homepage() {
   const [isTrue, setIsTrue] = useState(false);
-  const [balance, setBalance] = useState(0);
-  const [expense, setExpense] = useState(0);
-  const [budget, setBudget] = useState(0);
-  const [priceOfProduct, setPriceOfProduct] = useState(0);
-  const [nameOfProduct, setNameOfProduct] = useState("ahsan");
-  const [listOfTransaction, setListOfTransaction] = useState([
-    { priceOfProduct: 0, nameOfProduct: "ahsan" },
-    { priceOfProduct: 1, nameOfProduct: "haris" },
-  ]);
+  const [selectType , setSelectType] = useState("");
+  const [searchItem , setSearchItem] = useState("");
+  const [expense, setExpense] = useState(()=>{
+    return Number(localStorage.getItem('expense')) || 0;
+  });
+  const [budget, setBudget] = useState(()=>{
+    return Number(localStorage.getItem('budget')) || 0;
+  });
+  const [priceOfProduct, setPriceOfProduct] = useState();
+  const [nameOfProduct, setNameOfProduct] = useState("");
+  const [balance, setBalance] = useState(()=>{
+    return Number(localStorage.getItem('balance')) || 0;
+  });
+  const [listOfTransaction, setListOfTransaction] = useState(()=>{
+    return(
+      JSON.parse(localStorage.getItem('transaction')) ||[]
+    )
+  });
 
+
+  useEffect(()=>{
+    localStorage.setItem("transaction" , JSON.stringify(listOfTransaction));
+  },[listOfTransaction])
+
+  useEffect(()=>{
+    localStorage.setItem("balance" , balance);
+  } , [balance])
+
+  useEffect(()=>{
+    localStorage.setItem('expense' , expense);
+    localStorage.setItem('budget' , budget);
+  } , [expense , budget])
   function showTransactionContainer() {
     setIsTrue(isTrue ? false : true);
   }
   function addTransaction() {
-    setListOfTransaction([...listOfTransaction , {priceOfProduct , nameOfProduct}])
+    if(priceOfProduct == 0 || nameOfProduct == ''){
+      alert("all field are required");
+      return;
+    }
+    setListOfTransaction([...listOfTransaction , {priceOfProduct , nameOfProduct , type : selectType}])
+    setIsTrue(false);
+
+    if(selectType == 'budget'){
+      setBalance(Number(balance + priceOfProduct));
+      setBudget(budget + priceOfProduct)
+    }
+    else if(selectType == 'expense'){
+      setBalance(Number(balance - priceOfProduct));
+      setExpense(expense+priceOfProduct)
+    }
   }
 
   function deleteTransaction(deleteIdx){
@@ -25,13 +61,44 @@ function Homepage() {
     const updatedList = listOfTransaction.filter((_ , index) => index !== deleteIdx);
 
     setListOfTransaction(updatedList);
+    
+    if(itemToDelete.nameOfProduct.toLowerCase() == 'budget'){
+      setBalance(balance - itemToDelete.priceOfProduct);
+      if(budget > itemToDelete.priceOfProduct){
 
-    setBalance(balance - itemToDelete.priceOfProduct)
+        setBudget(budget - itemToDelete.priceOfProduct)
+      }
+    }
+    else{
+      setBalance(balance + itemToDelete.priceOfProduct);
+      setExpense(expense - itemToDelete.priceOfProduct)
+    }
   }
+
+  function clearAll(){
+    localStorage.clear();
+    setListOfTransaction([]);
+    setSelectType('');
+    setExpense(0);
+    setBalance(0);
+    setBudget(0);
+  }
+  const filteredTransaction = listOfTransaction.filter((item)=>{
+    return(
+      item.nameOfProduct.toLowerCase().includes(searchItem.toLowerCase()) ||
+      item.priceOfProduct.toString().includes(searchItem) ||
+      item.type.toLowerCase().includes(searchItem.toLowerCase())
+    )
+  })
+
+  
   return (
     <>
       <div className="hero-section">
-        <p className="name-of-web-app">Expense Tracker</p>
+        <div className="expense-tracker-header">
+          <p className="name-of-web-app">Expense Tracker</p>
+          <button className="clear-all-persistent-data" onClick={clearAll}>Clear all</button>
+        </div>
         <div className="balance-section">
           <span className="balance-amount">Balance : {balance}</span>
           {isTrue ? (
@@ -57,6 +124,9 @@ function Homepage() {
                 type="radio"
                 name="budget-and-expense"
                 id="expense-checked"
+                value={"expense"}
+                checked = {selectType == 'expense'}
+                onChange={(e) => setSelectType(e.target.value)}
               />
             </div>
             <div className="budget-check">
@@ -65,6 +135,9 @@ function Homepage() {
                 type="radio"
                 name="budget-and-expense"
                 id="budget-checked"
+                value={'budget'}
+                checked = {selectType == 'budget'}
+                onChange={(e)=>setSelectType(e.target.value)}
               />
             </div>
           </div>
@@ -85,17 +158,30 @@ function Homepage() {
 
         <div className="transaction-container">
           <p className="transaction-head">Transaction</p>
-          <input type="text" placeholder="search" />
+          <input type="text" placeholder="search" value={searchItem} onChange={(e) => setSearchItem(e.target.value)}/>
 
           <ul className="list-of-transaction">
-            {listOfTransaction.map((item , index) => {
+            {filteredTransaction.length === 0 ? <p>No Transaction Found</p> : 
+            filteredTransaction.map((item , index) => {
+              return(
+                 <li key={index}>
+                  <p>{item.nameOfProduct}</p> <p>{item.priceOfProduct}</p>{" "}
+                  <button onClick={()=>{deleteTransaction(index)}} className={item.type == 'expense' ? 'red-border': 'green-border'}>remove</button>
+                </li>
+              )
+            })
+
+            }
+
+
+            {/* {listOfTransaction.map((item , index) => {
               return (
                 <li key={index}>
                   <p>{item.nameOfProduct}</p> <p>{item.priceOfProduct}</p>{" "}
-                  <button onClick={()=>{deleteTransaction(index)}}>remove</button>
+                  <button onClick={()=>{deleteTransaction(index)}} className={item.type == 'expense' ? 'red-border': 'green-border'}>remove</button>
                 </li>
               );
-            })}
+            })} */}
           </ul>
         </div>
       </div>
