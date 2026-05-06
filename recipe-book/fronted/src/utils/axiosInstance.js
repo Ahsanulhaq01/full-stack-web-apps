3b043033337f2ca88c5a20498643f9daf17f88c9
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2026-05-06 15:33:24 +0500

    Add login page component with styles and images; include react-icons dependency

--- recipe-book/fronted/src/utils/axiosInstance.js
+++ recipe-book/fronted/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'https://full-stack-web-apps.onrender.com/api/v1',
+  baseURL: 'https://recipes-book-backend.onrender.com/api/v1',
   withCredentials: true,
 })
 
