579e47de25dd25cc74fc34b03afe4382abcd37f1
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2026-05-18 23:50:28 +0500

    feat: update CORS origin to allow requests from the production URL

--- GourmetKitchen/backend/src/app.js
+++ GourmetKitchen/backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://gourmetkitchen-one.vercel.app",
     credentials : true
 }));
 app.use(cookieParser())
