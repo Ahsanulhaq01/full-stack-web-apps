45bf8c4fb4ca2e78c0340d51d42d941ac7ae6187
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2026-06-06 23:27:12 +0500

    style: format code for consistency across recipe and user models; remove unnecessary whitespace in apiResponse

--- GourmetKitchen/backend/src/models/recipes.model.js
+++ GourmetKitchen/backend/src/models/recipes.model.js
@@ -1,56 +1,56 @@
 import mongoose from 'mongoose'
 
 const recipesSchema = new mongoose.Schema({
-    recipeTitle : {
-        type : String,
-        required : true,
-    },
-    description :{
-        type : String,
-        required : true,
-    },
-    difficulty : {
+    recipeTitle: {
         type: String,
-        enum : ["Easy" , "Intermediate" ,"Advance"],
-        required : true,
+        required: true,
     },
-    preparationTime : {
-        type:Number,
-        required : true,
+    description: {
+        type: String,
+        required: true,
     },
-    recipeImage : {
-        type : String,
-        required : true,
+    difficulty: {
+        type: String,
+        enum: ["Easy", "Intermediate", "Advance"],
+        required: true,
+    },
+    preparationTime: {
+        type: Number,
+        required: true,
+    },
+    recipeImage: {
+        type: String,
+        required: true,
 
     },
-    category : {
-        type : String ,
-        enum : ["Breakfast" , "Dinner" , "Lunch" ,"Dessert" , "Vegertarian"],
-        required : true,
+    category: {
+        type: String,
+        enum: ["Breakfast", "Dinner", "Lunch", "Dessert", "Vegertarian"],
+        required: true,
     },
-    ingrediant : {
-        type : [String],
-        required : true,
+    ingrediant: {
+        type: [String],
+        required: true,
 
     },
-    preparationStep : {
-        type : [String],
-        required : true,
+    preparationStep: {
+        type: [String],
+        required: true,
     },
-    isSaved : {
-        type : Boolean,
+    isSaved: {
+        type: Boolean,
     },
-    servings :{
-        type : Number , 
-        
+    servings: {
+        type: Number,
+
     },
-    createdBy : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
     }
 
-},{
-    timestamps : true,
+}, {
+    timestamps: true,
 })
 
-export const Recipe = mongoose.model('Recipe' , recipesSchema);
+export const Recipe = mongoose.model('Recipe', recipesSchema);
--- GourmetKitchen/backend/src/models/user.model.js
+++ GourmetKitchen/backend/src/models/user.model.js
@@ -3,71 +3,71 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
+    name: {
+        type: String,
+        required: true,
     },
-    email : {
-        type : String , 
-        unique : true  ,
-        lowercase : true,
-        trim : true,
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
-    password : {
-        type : String,
-        required :true,
+    password: {
+        type: String,
+        required: true,
         // minlength : 6,
     },
-    profileImage : {
-        type : String ,
-        default : "",
+    profileImage: {
+        type: String,
+        default: "",
 
     },
-    refreshToken : {
-        type : String,
+    refreshToken: {
+        type: String,
     }
-} , {timestamps : true})
+}, { timestamps: true })
 
 
-userSchema.pre('save' , async function (next) {
-    if(!this.isModified('password')) next;
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) next;
 
-    this.password = await bcrypt.hash(this.password , 10);
+    this.password = await bcrypt.hash(this.password, 10);
 
     next;
 })
 
 
-userSchema.methods.isPasswordCorrect = function (password){
-    return  bcrypt.compare(password , this.password)
+userSchema.methods.isPasswordCorrect = function (password) {
+    return bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
     return jwt.sign(
-       { 
-        _id : this._id,
-        email : this.email,
+        {
+            _id: this._id,
+            email: this.email,
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn :process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 };
 
 
-userSchema.methods.generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
-            _id : this._id,
+            _id: this._id,
         },
         process.env.RREFRESH_TOKEN_SECRET,
         {
-            expiresIn : process.env.RREFRESH_TOKEN_EXPIRY
+            expiresIn: process.env.RREFRESH_TOKEN_EXPIRY
         }
     )
 }
 
-const User = mongoose.model('User' , userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
